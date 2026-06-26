14abb4ee83a7b1471de90694c831fbe0f756e420
Mohammed Eid <meid5170@gmail.com>
2026-06-26 20:41:14 +0300

    Add BaseEntity and Role entity

---
+++ src/main/java/com/eventhub/entity/Role.java
@@ -0,0 +1,23 @@
+package com.eventhub.entity;
+
+import com.eventhub.entity.base.BaseEntity;
+import com.eventhub.enums.RoleType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role extends BaseEntity {
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleType name;
+
+}
---
+++ src/main/java/com/eventhub/enums/RoleType.java
@@ -0,0 +1,7 @@
+package com.eventhub.enums;
+
+public enum RoleType {
+    ADMIN,
+    ORGANIZER,
+    USER
+}
