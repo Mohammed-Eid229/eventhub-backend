94cfcb09ffca927d7759d3c649ca6927d584c347
Mohammed Eid <meid5170@gmail.com>
2026-06-26 21:27:57 +0300

    Implement user and role persistence layer

---
+++ src/main/java/com/eventhub/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.eventhub.repository;
+
+import com.eventhub.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
